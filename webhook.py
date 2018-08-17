cb5a128107c771a1d2a7c0a5f01d97c38fb2b6b4
rubenbo <bo.ruben@gmail.com>
2018-08-17 12:04:25 +0200

    Add files via upload

---
+++ webhook.py
@@ -0,0 +1,41 @@
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+    
+    res = makeResponse(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeResponse(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    orgnr = parameters.get("orgnr")    
+    r = requests.get('https://data.brreg.no/enhetsregisteret/api/enheter/'+orgnr)
+    json_object = r.json()
+    speech = "The name of "+orgnr+" is "+json_object['navn']
+    return {
+    "speech": speech,
+    "displayText": speech,
+    "source": "brreg-webhook-nameof"
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
